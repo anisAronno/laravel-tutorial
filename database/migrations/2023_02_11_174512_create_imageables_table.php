6d15e166e6f641e47e84cf17c65255929b0974f3
MD. Anichur Rahaman <38912435+anisAronno@users.noreply.github.com>
2023-10-18 21:37:21 +0600

    Code move to root folder

--- database/migrations/2023_02_11_174512_create_imageables_table.php
+++ database/migrations/2023_02_11_174512_create_imageables_table.php
@@ -1,6 +1,6 @@
 <?php
 
-use AnisAronno\MediaHelper\Models\Image;
+use AnisAronno\MediaGallery\Models\Image;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
